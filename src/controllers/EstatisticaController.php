f4e744e24a32ea33b711bae9880873a9f586cb1e
Joeverson Santana <joeversonsantana@gmail.com>
2025-06-05 21:30:32 -0300

    criação do /estatisticas

---
+++ src/controllers/EstatisticaController.php
@@ -0,0 +1,25 @@
+<?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+
+class EstatisticaController {
+    private $estatisticaService;
+    
+    public function __construct() {
+        $this->estatisticaService = new EstatisticaService();
+    }
+    
+    public function calcular(Request $request, Response $response) {
+        try {
+            $estatisticas = $this->estatisticaService->calcularEstatisticas();
+            
+            $response->getBody()->write(json_encode($estatisticas));
+            return $response->withStatus(200);
+            
+        } catch (Exception $e) {
+            error_log("Erro ao calcular estatísticas: " . $e->getMessage());
+            return $response->withStatus(500);
+        }
+    }
+}
